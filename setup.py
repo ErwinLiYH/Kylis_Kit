575315d616895da46384aded91771fb26779d895
ErwinLi <60261171+ErwinLiYH@users.noreply.github.com>
2022-08-19 10:08:41 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Kkit",
-    version="1.2.0",
+    version="1.3.0",
     author="Kylis",
     author_email="1779599839@qq.com",
     description="some personnal kits",
