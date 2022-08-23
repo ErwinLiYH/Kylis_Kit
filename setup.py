7ca90b0e0e32ac840b1dde2a85a3dade921ce333
Erwin <1779599839@qq.com>
2022-08-23 15:47:18 +0800

    version 2.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Kkit",
-    version="2.1.0",
+    version="2.1.1",
     author="Kylis",
     author_email="1779599839@qq.com",
     description="some personnal kits",
--- src/Kkit/__init__.py
+++ src/Kkit/__init__.py
@@ -1,3 +1,2 @@
 from . import *
-from utils import *
-
+from .utils import *
