b1c29f0cde9092cef8b0136de0a022f7876f33e8
Erwin <1779599839@qq.com>
2022-08-23 14:14:41 +0800

    version 2.0.0

--- src/Kkit/__init__.py
+++ src/Kkit/__init__.py
@@ -1,26 +1,3 @@
 from . import *
+from utils import *
 
-def print_list(Alist, num_of_columns=None, separator_in_line=" , ", separator_between_line="\n", prefix="", verbose=True):
-    length = len(Alist)
-    if num_of_columns == None:
-        num_of_columns = length
-    print(prefix, end="")
-    for i in range(length):
-        if i%num_of_columns != (num_of_columns-1):
-            if i == (length-1):
-                print(Alist[i], end = separator_between_line)
-            else:
-                print(Alist[i], end = separator_in_line)
-        else:
-            print(Alist[i], end = separator_between_line)
-    if verbose:
-        print("\nlength: %d"%length)
-
-def conclude_list(Alist):
-    result = {}
-    for i in Alist:
-        if i in result:
-            result[i] += 1
-        else:
-            result[i] = 1
-    return result
