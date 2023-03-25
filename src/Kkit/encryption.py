3f9e95dbe880ad088b0ee633b0bad0151b7a24af
Yuanhao Li <erwinli@qq.com>
2023-03-25 00:59:39 +0000

    add encryption module

---
+++ src/Kkit/encryption.py
@@ -0,0 +1,21 @@
+import hashlib
+import base64
+from cryptography.fernet import Fernet
+
+def gen_key(pin):
+    bytes_key = pin.encode('utf-8')
+    hash_object = hashlib.sha256(bytes_key)
+    hex_dig = hash_object.hexdigest()
+    key_bytes = bytes.fromhex(hex_dig)[:32]
+    key = base64.urlsafe_b64encode(key_bytes)
+    return key
+
+def encrypt_string(string, key, encoding="utf-8"):
+    key = gen_key(key)
+    cipher_suite = Fernet(key)
+    return cipher_suite.encrypt(string.encode(encoding)).decode(encoding)
+
+def decrypt_string(string, key, encoding="utf-8"):
+    key = gen_key(key)
+    cipher_suite = Fernet(key)
+    return cipher_suite.decrypt(string.encode(encoding)).decode(encoding)
