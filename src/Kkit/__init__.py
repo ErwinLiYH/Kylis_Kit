22cb5f5c4ff669de1f35d7b796b68eefebd9ed8c
Yuanhao Li <erwinli@qq.com>
2024-03-21 18:58:57 +0000

    update main page doc

--- src/Kkit/__init__.py
+++ src/Kkit/__init__.py
@@ -1,6 +1,20 @@
 """
+# Kylis kit
+
 Some personal tools for python programming.
 
 More information can be found at the help of each sub-module.
+
+## Install and import
+
+```bash
+pip install git+https://github.com/erwinliyh/kylis_kit@main
+```
+
+```python
+import Kkit
+# or
+from Kkit import <module>
+```
 """
 from .utils import *
