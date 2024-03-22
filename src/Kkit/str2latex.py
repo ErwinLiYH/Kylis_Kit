1445d50671431edf0e679867f4cd5b1f42a22f4c
Yuanhao Li <erwinli@qq.com>
2024-03-22 13:23:00 +0000

    update document in code

--- src/Kkit/str2latex.py
+++ src/Kkit/str2latex.py
@@ -1,3 +1,35 @@
+"""
+This module is used to transfer between LaTex bmatrix and numpy array.
+
+Example:
+
+```python
+from Kkit import str2latex
+
+bmatrix = '''
+\\begin{bmatrix}
+1 & 2 & 3 \\\\
+4 & 5 & 6
+\\end{bmatrix}
+'''
+
+array = str2latex.bmatrix2numpy(bmatrix)
+
+print(array)
+# [[1 2 3]
+#  [4 5 6]]
+
+bmatrix = str2latex.numpy2bmatrix(array)
+
+print(bmatrix)
+
+# \begin{bmatrix}
+# 1&2&3\\
+# 4&5&6
+# \end{bmatrix}
+```
+"""
+
 # Transfer between string and object
 # Author:walkureHHH
 # Last modify:2020/10/14
@@ -7,6 +39,19 @@ class latex_str_error(Exception):
     pass
 
 def bmatrix2numpy(latex_str)->numpy.array:
+    """
+    Transfer a latex bmatrix to a numpy array
+
+    Parameters:
+    --------------
+    latex_str: str
+        The latex string
+
+    Returns:
+    --------------
+    numpy.array
+        The numpy array
+    """
     latex_str = latex_str.strip()
     if latex_str.startswith('\\begin{bmatrix}') and latex_str.endswith('\\end{bmatrix}'):
         latex_str = latex_str.replace('\\begin{bmatrix}','').replace('\\end{bmatrix}','')
@@ -17,7 +62,20 @@ def bmatrix2numpy(latex_str)->numpy.array:
         raise latex_str_error("this is not a bmatrix,please check it again")
 
 def numpy2bmatrix(array)->str:
-    s = '\\begin{bmatrix}'
+    """
+    Transfer a numpy array to a latex bmatrix
+
+    Parameters:
+    --------------
+    array: numpy.array
+        The numpy array
+
+    Returns:
+    --------------
+    str
+        The latex string
+    """
+    s = '\\begin{bmatrix}\n'
     shape = numpy.shape(array)
     str_list = array.tolist()
     for i in range(0,shape[0]):
@@ -27,25 +85,25 @@ def numpy2bmatrix(array)->str:
             else:
                 s += str(str_list[i][j])
         if i < shape[0]-1:
-            s += '\\\\'
+            s += '\\\\\n'
         else:
-            s += '\\end{bmatrix}'
+            s += '\n\\end{bmatrix}'
     return s
 
-def str2list(list_str,mod_str = ',')->list:
-    list_str = list_str.strip().replace('\n',mod_str)
-    return list_str.split(mod_str)
+# def str2list(list_str,mod_str = ',')->list:
+#     list_str = list_str.strip().replace('\n',mod_str)
+#     return list_str.split(mod_str)
 
-def list2str(list_,mod_str=',')->str:
-    result = ''
-    for i in range(0,len(list_)):
-        if i < len(list_)-1:
-            result += (str(list_[i])+mod_str)
-        else:
-            result += str(list_[i])
-    return "[%s]"%result
+# def list2str(list_,mod_str=',')->str:
+#     result = ''
+#     for i in range(0,len(list_)):
+#         if i < len(list_)-1:
+#             result += (str(list_[i])+mod_str)
+#         else:
+#             result += str(list_[i])
+#     return "[%s]"%result
 
-if __name__ == "__main__":
-    lista = [1,2,3,4,5,6]
-    print(lista)
-    print(list2str(lista))
+# if __name__ == "__main__":
+#     lista = [1,2,3,4,5,6]
+#     print(lista)
+#     print(list2str(lista))
