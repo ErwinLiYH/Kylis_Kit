51c1262eac3e28b853b2e5494e1201b502c30977
ErwinLiYH <erwinli@qq.com>
2025-03-05 17:19:50 +0000

    change default port

--- src/Kkit/llm_utils/llamacpp_wrapper_server.py
+++ src/Kkit/llm_utils/llamacpp_wrapper_server.py
@@ -169,6 +169,6 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0", help="Server listening host")
-    parser.add_argument("--port", type=int, default=8000, help="Server listening port")
+    parser.add_argument("--port", type=int, default=8001, help="Server listening port")
     args = parser.parse_args()
     uvicorn.run(app, host="0.0.0.0", port=args.port)
